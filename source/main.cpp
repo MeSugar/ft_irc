04799036ac176ca059332dbb051c6f8a1f5844ef
Eugene Vlasov <vlasov3454@yandex.ru>
2022-01-07 20:24:57 +0300

    PASScmd & some basic methods added

--- source/main.cpp
+++ source/main.cpp
@@ -1,5 +1,19 @@
 #include "../include/Server.hpp"
 
+// void	testPASScmd(Server &serv)
+// {
+// 	Client client;
+// 	std::vector<std::string> vec1;
+// 	std::vector<std::string> vec2;
+// 	vec1.push_back("bbb");
+// 	vec1.push_back("ccc");
+// 	Message msg1("aaa", "PASS", vec1);
+// 	Message msg2(std::string(), "PASS", vec1);
+// 	serv.commandPASS(client, msg1);
+// 	serv.commandPASS(client, msg2);
+	
+// }
+
 int main(int ac, char **av)
 {
 	if (ac == 3)
@@ -9,6 +23,7 @@ int main(int ac, char **av)
 		int num;
 		ss >> num;
 		Server serv(num, av[2]);
+		// testPASScmd(serv);
 		return 0;
 	}
 	std::cout << "Wrong number of arguments" << std::endl;
