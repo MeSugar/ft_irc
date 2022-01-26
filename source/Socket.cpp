c9c55481d7e89fe393078637d0b6cd58ad7abf31
airon <83311717+vilinovv@users.noreply.github.com>
2022-01-26 21:56:59 +0300

    socket changed to ipv4

--- source/Socket.cpp
+++ source/Socket.cpp
@@ -114,7 +114,7 @@ int Socket::_getaddrinfo() {
 
     bzero(&hints, sizeof(hints));
    
-    hints.ai_family = AF_UNSPEC; 
+    hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
     // Заполняю res
